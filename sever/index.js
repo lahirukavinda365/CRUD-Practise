1daecfeac84183f772fcdca0c5fddac45aae7452
Lahiru Karunarathna <lahirukavinda365@gmail.com>
2023-08-11 21:20:05 +0530

    update user detail functionality completed.

--- sever/index.js
+++ sever/index.js
@@ -26,15 +26,34 @@ const dbConnect = module.exports = () => {
 dbConnect();
 
 // CRUD 
-
+//get request get users
 app.get('/', (req,res) => {
     UserModel.find({})
     .then(users => res.json(users))
     .catch(err => res.json(err))
 })
 
+//get request to get a user
 
+app.get('/getUser/:id', (req,res) => {
+    const id = req.params.id;
 
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.put('/updateUser/:id', (req, res) => {
+    const id = req.params.id;
+
+    UserModel.findByIdAndUpdate({_id:id}, {
+        name:req.body.name,
+        email:req.body.email,
+        age:req.body.age
+    })
+    .then( users => res.json(users))
+    .catch(err => res.json(err))
+})
 
 
 
